6d8b3d792985ebd19ed53cf96986183c52bbee82
meta-PC <1427662743@qq.com>
2020-03-03 20:20:20 +0800

    Update img_to_h5.py

--- processing/img_to_h5.py
+++ processing/img_to_h5.py
@@ -13,36 +13,27 @@ import numpy as np
 import h5py
 
 
+img_dir = r'../dataSet/Images/generate_pic'
+out_h5 = r'../dataSet/cache'
+
+
 # 图片转h5文件
 def image_to_h5():
-    dirs = os.listdir("resized_img")
+    dirs = os.listdir(img_dir)
     Y = []  # label
     X = []  # data
     print(len(dirs))
     for filename in dirs:
         label = int(filename.split('_')[0])
         Y.append(label)
-        im = Image.open("resized_img/{}".format(filename)).convert('RGB')
+        im = Image.open(os.path.join(img_dir, filename)).convert('RGB')
         mat = np.asarray(im)  # image 转矩阵
         X.append(mat)
 
-    if not os.path.exists('dataset'):
-        os.mkdir('dataset')
-
-    file = h5py.File("dataset/data.h5", "w")
+    file = h5py.File(os.path.join(out_h5, "data.h5"), "w")
     file.create_dataset('X', data=np.array(X))
     file.create_dataset('Y', data=np.array(Y))
     file.close()
-
-
-# test
-# data = h5py.File("dataset//data.h5","r")
-# X_data = data['X']
-# print(X_data.shape)
-# Y_data = data['Y']
-# print(Y_data[123])
-# image = Image.fromarray(X_data[123]) #矩阵转图片并显示
-# image.show()
 
 
 if __name__ == "__main__":
